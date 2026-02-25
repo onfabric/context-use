0a564236a9883b20d4223fe1dc316742fd40f2b8
Alex Kulikov <7394728+alexeyqu@users.noreply.github.com>
2026-02-25 16:40:20 +0000

    chore: rm stale ETL Loader classes (#73)
    `Loader` is superseded with `Store` and nothing uses it now

--- context_use/etl/core/__init__.py
+++ context_use/etl/core/__init__.py
@@ -2,14 +2,11 @@ from context_use.etl.core.exceptions import (
     ArchiveProcessingError,
     UnsupportedProviderError,
 )
-from context_use.etl.core.loader import DbLoader, Loader
 from context_use.etl.core.pipe import Pipe
 from context_use.etl.core.types import ThreadRow
 
 __all__ = [
     "ArchiveProcessingError",
-    "DbLoader",
-    "Loader",
     "Pipe",
     "ThreadRow",
     "UnsupportedProviderError",
