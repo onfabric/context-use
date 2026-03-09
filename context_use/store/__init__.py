b73667c9d329b82e9a2c18dc947adeae70b541a9
Luca Bertelli <luca@onfabric.io>
2026-03-09 18:41:06 +0000

    feat!: remove in-memory store (#132)
    Follow-up of #128.
    
    Blocked by #131.

--- context_use/store/__init__.py
+++ context_use/store/__init__.py
@@ -1,9 +1,7 @@
 from context_use.store.base import MemorySearchResult, Store
-from context_use.store.memory import InMemoryStore
 from context_use.store.sqlite import SqliteStore
 
 __all__ = [
-    "InMemoryStore",
     "MemorySearchResult",
     "SqliteStore",
     "Store",
